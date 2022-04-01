97f61d103273ab85a415fa0fe0b0be0c06d77605
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-03-31 21:05:10 -0700

    Format & Display Timer logic in DOM

--- script.js
+++ script.js
@@ -40,6 +40,39 @@ let finalTimeDisplay = '0.0s';
 // Scroll
 let valueY = 0;
 
+// Reset Game
+function playAgain() {
+  gamePage.addEventListener('click', startTimer);
+  scorePage.hidden = true;
+  splashPage.hidden = false;
+  equationsArray = [];
+  playerGuessArray = [];
+  valueY = 0;
+  playAgainBtn.hidden = true;
+}
+
+// Show Score Page
+function showScorePage() {
+  setTimeout(() => {
+    playAgainBtn.hidden = false;
+  }, 1000);
+  gamePage.hidden = true;
+  scorePage.hidden = false
+}
+
+// Format & Display Time in DOM
+function scoresToDOM() {
+  finalTimeDisplay = finalTime.toFixed(1);
+  baseTime = timePlayed.toFixed(1);
+  penaltyTime = penaltyTime.toFixed(1);
+  baseTimeEl.textContent = `Base Time: ${baseTime}`;
+  penaltyTimeEl.textContent = `Penalty : ${penaltyTime}s`;
+  finalTimeEl.textContent = `${finalTimeDisplay}s`;
+  itemContainer.scrollTo({ top: 0, behavior: 'instant'})
+  showScorePage();
+}
+
+
 // Stop Timer, Process Results, go to Score Page
 function checkTime() {
   // console.log(timePlayed);
@@ -59,6 +92,7 @@ function checkTime() {
     }
     finalTime = timePlayed + penaltyTime;
     console.log('time', timePlayed, 'penalty', penaltyTime, 'final', finalTime);
+    scoresToDOM()
   }
   
 }
